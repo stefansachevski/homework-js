55c49c5913875f8c3777aa15399a8d19baf36b85
stefansachevski <stefansacevski10@gmail.com>
2023-12-05 15:21:49 +0100

    update homework1

--- Homework 1/script.js
+++ Homework 1/script.js
@@ -1,3 +1,4 @@
 let phonePrice = 119.95;
-let result= 30 * ((5/100)*phonePrice);
+let tax = ((5/100)*phonePrice)
+let result= 30 * (phonePrice+tax)
 console.log ('Price for 30 phones with 5% taxe is' , result);
